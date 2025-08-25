176400f426d431ef40aad05c8f81f21050843bec
Chris Buckley <chris@cmbuckley.co.uk>
2025-08-25 23:17:53 +0100

    Update healthcheck.js

--- .github/scripts/healthcheck.js
+++ .github/scripts/healthcheck.js
@@ -7,6 +7,7 @@ module.exports = async (core) => {
             MONITOR_DOMAINS = '',
             ERROR_THRESHOLD = 7,
             WARN_THRESHOLD = 28,
+            TIMEOUT = 5000,
             RETRY_ATTEMPTS = 3,
             RETRY_DELAY = 500,
         } = process.env,
@@ -14,7 +15,13 @@ module.exports = async (core) => {
 
     async function check(options) {
         return new Promise(resolve => {
-            https.get(options, function (response) {
+            https.get({
+                hostname: options.hostname,
+                timeout: options.timeout,
+                headers: {
+                    host: options.domain,
+                },
+            }, function (response) {
                 const cert = response.socket.getPeerCertificate();
                 this.abort(); // cancel the connection
 
@@ -23,23 +30,30 @@ module.exports = async (core) => {
                     core.setFailed();
                 } else {
                     const daysLeft = parseInt((Date.parse(cert.valid_to) - new Date().getTime()) / (1000 * 60 * 60 * 24), 10),
-                        level = (daysLeft < ERROR_THRESHOLD ? 'error' : (daysLeft < WARN_THRESHOLD ? 'warning' : 'info')),
-                        colour = '\u001b[' + (daysLeft < ERROR_THRESHOLD ? 31 : (daysLeft < WARN_THRESHOLD ? 33 : 32)) + 'm',
+                        level = (daysLeft < options.errorThreshold ? 'error' : (daysLeft < options.warnThreshold ? 'warning' : 'info')),
+                        colour = '\u001b[' + (daysLeft < options.errorThreshold ? 31 : (daysLeft < options.warnThreshold ? 33 : 32)) + 'm',
                         reset = '\u001b[0m';
 
                     core[level](`Certificate for ${options.domain} expires in ${colour}${daysLeft}${reset} days (${cert.valid_to})`);
 
                     if (level == 'warning') {
-                        warnings.push({domain: options.domain, daysLeft, date: cert.valid_to});
+                        warnings.push({
+                            domain: options.domain,
+                            daysLeft,
+                            date: cert.valid_to,
+                        });
                     }
 
-                    if (daysLeft < ERROR_THRESHOLD) {
+                    if (daysLeft < options.errorThreshold) {
                         core.setFailed('Certificates expire soon');
                     }
                 }
 
                 resolve();
             }).on('error', function (e) {
+                if (!options.attempts) options.attempts = 1;
+                core.info(`Error connecting to ${options.domain} (attempt ${options.attempts}): ${e.message}`);
+
                 if (options.attempts < options.maxRetries) {
                     setTimeout(() => {
                         check(Object.assign({}, options, {
@@ -47,7 +61,7 @@ module.exports = async (core) => {
                         })).then(resolve);
                     }, options.retryDelay);
                 } else {
-                    core.error(`Error connecting to ${options.domain} after ${options.attempts} attempts: ${e.message}`);
+                    core.error(`Error connecting to ${options.domain} after ${options.attempts} attempts`);
                     core.setFailed();
                     resolve();
                 }
@@ -57,13 +71,13 @@ module.exports = async (core) => {
 
     for (const domain of MONITOR_DOMAINS.split('|').filter(Boolean)) {
         await check({
+            domain,
             hostname: HOST,
-            attempts: 1,
+            timeout: TIMEOUT,
+            errorThreshold: ERROR_THRESHOLD,
+            warnThreshold: WARN_THRESHOLD,
             maxRetries: RETRY_ATTEMPTS,
             retryDelay: RETRY_DELAY,
-            headers: {
-                host: domain,
-            },
         });
     }
 
